b4ea66cecb7d9c32a8f9bc627bb98d063c623041
Javier Plumey <javierplumey@gmail.com>
2012-04-28 19:06:12 -0400

    Added some static pages.

--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+  	before {visit root_path}
 
-    it "should have the content 'OLPH School Volunteer Center'" do
-      visit '/static_pages/home'
-      page.should have_content('OLPH School Volunteer Center')
+  	it {should have_selector('h1', text:'OLPH School Volunteer Center')}
+
+
     end
-  end
+
+ 
+
+
+  
 end
