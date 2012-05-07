319076f8a2c01876d1e0dc9be9c175969ab86088
Javier Plumey <javierplumey@gmail.com>
2012-05-07 11:53:32 -0400

    Changed admin to default to false

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120502164957) do
+ActiveRecord::Schema.define(:version => 20120507153302) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
     t.string   "year"
-    t.boolean  "admin"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.boolean  "admin",           :default => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "email"
     t.string   "password_digest"
     t.string   "remember_token"
