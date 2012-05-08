a954f3581c89f71106de1f624757dfdf8376ddbc
Javier Plumey <javierplumey@gmail.com>
2012-05-08 13:40:25 -0400

    Ensure postings cannot be added for future dates.

--- app/models/volunteer_event.rb
+++ app/models/volunteer_event.rb
@@ -1,13 +1,15 @@
 class VolunteerEvent < ActiveRecord::Base
   attr_accessible :event_date, :hours_worked, :name
 
+
   validates	:name, presence: true,
   									length: {maximum: 80}
   validates :hours_worked, presence: true,
   									numericality: {only_integer: true}
   validates :event_date, presence: true
-  validates_date	:event_date
+  validates_date	:event_date, :on_or_before => lambda { Date.current }
   
+
 
   belongs_to :worker
   #belongs_to :eventtype
