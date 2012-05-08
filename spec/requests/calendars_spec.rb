077022bd191265749304fb93c74bb240193966f2
Javier Plumey <javierplumey@gmail.com>
2012-05-08 11:54:43 -0400

    Added base calendar files.

---
+++ spec/requests/calendars_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "Volunteer Calendar pages" do
+ 
+	subject {page}
+
+	describe "Volunteer Calendar sign up calendar" do
+
+
+		describe "should not show if not logged in" do
+			visit calendar_path
+		end
+
+		decribe "should show calendar" do
+
+		end
+
+
+	end
+
+end
