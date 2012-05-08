01807441354fe2020e0a6ce5924c9e069cc678a9
Javier Plumey <javierplumey@gmail.com>
2012-05-08 13:40:43 -0400

    Redirect logged in user to own account page when hitting 'Home'

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
+	
+	include SessionsHelper
+  
+  before_filter :signed_in_account, only: [:home]
+
   def home
+
+  	if signed_in? 
+			redirect_to current_account
+		end	
+
   end
 
   def help
