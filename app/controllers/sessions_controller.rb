c6b069cf6aafce9469c74849cc880a627bd0e6bf
Javier Plumey <javierplumey@gmail.com>
2013-03-27 08:31:54 -0400

    Fixed issue 11 regarding logon with case

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,10 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		account = Account.find_by_email(params[:session][:email])
+		 #Fixing defect found when user inputs email with uppercase in logon
+    account = Account.where('lower(email) = ?' , params[:session][:email].downcase).first
+
+	
 		if account && account.authenticate(params[:session][:password])
 			sign_in account
 			redirect_back_or account
